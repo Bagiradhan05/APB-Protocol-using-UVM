e845e0444b6d39cad9ad45fc6f1ba9a49ab4556f
BAGIRADHAN S <bagiradhansrinivasan@gmail.com>
2026-06-08 21:58:59 +0530

    Create files.f

---
+++ Run/files.f
@@ -0,0 +1,14 @@
++incdir+/home/dvft1104/final_apb_uvm/verification/agent/
++incdir+/home/dvft1104/final_apb_uvm/verification/env/
++incdir+/home/dvft1104/final_apb_uvm/verification/test/
++incdir+/home/dvft1104/final_apb_uvm/verification/tb/
+
+
+/home/dvft1104/final_apb_uvm/verification/tb/apb_interface.sv
+
+/home/dvft1104/final_apb_uvm/verification/agent/apb_agent_package.svp
+/home/dvft1104/final_apb_uvm/verification/env/apb_env_package.svp
+/home/dvft1104/final_apb_uvm/verification/test/apb_test_package.svp
+
+/home/dvft1104/final_apb_uvm/design/apb_slave_design.sv
+/home/dvft1104/final_apb_uvm/verification/tb/apb_top.sv
